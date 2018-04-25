cb3307d14be342212a327224cc5f725e651adc84
Captain <mads130f@edu.easj.dk>
2018-04-25 08:54:25 +0200

    eksdee

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+
+        System.out.println("Hello");
+        System.out.println("resultatet er " + factorial(5)+" N o");
+        System.out.println("Fiber " + fibernachi(10));
+    }
+
+    private static int factorial(int n){
+
+            if(n==1){
+                return 1;
+            }
+            else{
+                return n * factorial((n-1));
+            }
 
     }
+
+
+
+    static int fibernachi(int n){
+
+        if(n<2){
+            return 1;
+        }
+        else{
+            return (fibernachi(n-1)+fibernachi(n-2));
+        }
+
+    }
+
+
+
 }
